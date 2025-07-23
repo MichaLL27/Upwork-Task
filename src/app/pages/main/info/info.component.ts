81cbb08c87dad116ace9f89b37e09e09364ed218
MichaLL27 <lazarashvili2704@gmail.com>
2025-07-23 16:36:41 +0400

    create main info

--- src/app/pages/main/info/info.component.ts
+++ src/app/pages/main/info/info.component.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'upwork-header',
+  selector: 'upwork-info',
   standalone: true, 
   imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class InfoComponent {
 
 }
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-main',
-  imports: [],
-  templateUrl: './main.html',
-  styleUrl: './main.scss'
+  selector: 'upwork-main',
+  standalone: true, 
+  imports: [CommonModule, RouterModule],
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'], 
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Main {
+export class MainComponent {
 
 }
