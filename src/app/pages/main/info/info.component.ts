a40774d99be4338d633dbccdb86eecc1ded21778
MichaLL27 <lazarashvili2704@gmail.com>
2025-07-23 16:47:46 +0400

    almost finish info component

--- src/app/pages/main/info/info.component.ts
+++ src/app/pages/main/info/info.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { delay, of } from 'rxjs';
 
 @Component({
   selector: 'upwork-info',
   standalone: true, 
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoComponent {
-
+ information$ = of([
+    {
+      name: 'סה״כ הגשות שאלון',
+      number: 259,
+    },
+    {
+      name: 'סה״כ כניסות לשאלון',
+      number: 259,
+    },
+    {
+      name: 'סה״כ בתי ספר',
+      number: 504,
+    },
+  ]).pipe(delay(1000)); // thats a imitaion of a real API call
 }
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { InfoComponent } from './info/info.component';
 
 @Component({
   selector: 'upwork-main',
   standalone: true, 
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule,  InfoComponent],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'], 
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {
+
 
 }
