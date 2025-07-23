14ab63cd60ed9b730caaf7447bb44c5b92160908
MichaLL27 <lazarashvili2704@gmail.com>
2025-07-23 16:18:29 +0400

    create header and main page

---
+++ src/app/core/layout/layout.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'upwork-layout',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LayoutComponent {
+
+}
